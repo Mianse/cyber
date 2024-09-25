eb87f256c2358605398297e277d9eeb67456031b
damian orina <96182161+Mianse@users.noreply.github.com>
2024-09-25 10:49:25 +0300

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,7 +6,7 @@ import { AppText, skillsList, workDetail } from './../Constants'
 
 const Skills = () => {
   return (
-    <section>
+    <section id="skills">
     <div className="mx-auto max-w-screen-2xl px-4 py-8 sm:px-6 lg:px-8">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
         <div className="bg-blue-600 p-8 md:p-12 lg:px-16 lg:py-24">
