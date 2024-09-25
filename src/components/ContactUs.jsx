01f9b353dea54a203dc93cef51363ebef308bb30
damian orina <96182161+Mianse@users.noreply.github.com>
2024-09-25 11:34:59 +0300

    Update ContactUs.jsx

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -43,7 +43,12 @@ function ContactUs() {
       <label className="block text-sm font-medium leading-6 text-gray-900">Message</label>
       <textarea  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
  name="message" />
-      <input value='send'type="submit"  className='transition-all ease-in-out hover:scale-110 bg-blue-500 flex flex-row items-center justify-center text-[14px]  py-1 mt-5 text-white  rounded-md' />
+              <button
+                  type="button"
+                  className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                >
+                  Change
+                </button>
     </form>
   </div>
  </div>   
